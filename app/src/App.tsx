516b34cc25f68d2b1a1c47592315f420da109467
jivko25 <jivkou@yahoo.com>
2022-01-24 00:47:43 +0200

    Change App.tsx component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,17 +1,27 @@
 import { useState } from 'react';
-import './App.scss'
-import { NewTaskButton } from './components/new-task-button/NewTaskButton';
+import styles from './App.module.scss'
+// import { Modal } from './components/modal/Modal';
+import { Button } from './components/button/Button';
 import { TaskWrapper } from './components/task-wrapper/TaskWrapper';
 
 function App() {
   const [onOpenCreate, setOnOpenCreate] = useState(false);
+  const test = <h1>testove</h1>
+  const actions = <Button onNewTask={() => {setOnOpenCreate(false);}} title={'Add task'}/>
   return (
-    <div className='wrapper'>
-        <TaskWrapper title={"To Do"} content={{'test' : 1}}/>
-        <TaskWrapper title={"In progress"} content={{'test' : 1}}/>
-        <TaskWrapper title={"Done"} content={{'test' : 1}}/>
-        <NewTaskButton onNewTask={() => {setOnOpenCreate(true);
-        }}/>
+    <div>
+      <div className={!onOpenCreate ? styles.wrapper : styles['wrapper-blur']}>
+          <TaskWrapper title={"To Do"} content={{'test' : 1}}/>
+          <TaskWrapper title={"In progress"} content={{'test' : 1}}/>
+          <TaskWrapper title={"Done"} content={{'test' : 1}}/>
+          <Button onNewTask={() => {setOnOpenCreate(true);}} title={'New task'}/>
+      </div>
+      <div className={styles.modal}>
+        {/* {onOpenCreate &&
+          // <Modal title="test" content={test} actions={actions} isOpen={true}/>
+        } */}
+
+      </div>
     </div>
   );
 }

70ccbfa04c1404f69dcd69d03aed98e0689f9cd5
jivko25 <jivkou@yahoo.com>
2022-01-25 02:56:39 +0200

    Remove arrows

--- app/src/components/task/Task.tsx
+++ app/src/components/task/Task.tsx
@@ -14,12 +14,6 @@ export const Task: React.FC<Props> = ({id, title, description, assignee, status}
             <h4>Title:&ensp;{title}</h4>
             <h4>Description:&ensp;{description}</h4>
             <h4>Assignee:&ensp;{assignee}</h4>
-            {status == 'In Progress' &&
-            <h1 className={styles.leftArrow}>&lArr;</h1>
-            }
-            {status == 'In Progress' || status == 'To Do' &&
-            <h1 className={styles.rightArrow}>&rArr;</h1>
-            }
         </div>
     )
 }
